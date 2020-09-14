f791d43cd4ffcdd5db4d4a989a59351c26a9c656
Peter Vennemann <vennemann@fh-muenster.de>
2020-09-14 19:36:02 +0200

    Corrected computron path.

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(ß, os.path.abspath('./sphinx_computron')
 
 # -- Project information -----------------------------------------------------
 
